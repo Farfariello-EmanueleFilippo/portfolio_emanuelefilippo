12c111a4d43bbba46c6cef9bb58abf64c8bcbeb5
farfariello-emanuelefilippo <emanuelefilippo.farfariello@istitutoagnelli.it>
2024-05-27 13:14:43 +0100

    portfolio

---
+++ emanuele_filippo_portfolio/src/routes/quinta/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+    import Nav from "../../lib/components/nav.svelte";
+    import PdfViewr from "../../lib/components/pdf_viewr.svelte";
+</script>
+
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>La mia descrizione</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 18px;
+            background-color: #f4f4f4;
+            color: #333;
+            line-height: 1.6;
+            margin: 0;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .container {
+            text-align: center;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 800px;
+            margin: auto;
+        }
+        h2 {
+            font-family: "Georgia", serif;
+            font-size: 32px;
+            color: #5D5C61;
+            margin-bottom: 20px;
+        }
+        h4 {
+            font-family: "Georgia", serif;
+            font-size: 32px;
+            color: #5D5C61;
+            margin-bottom: 20px;
+        }
+        p {
+            margin: 20px 0;
+        }
+       
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2> anno 2023 - 2024 </h2>
+        <p>
+            Nel quarto anno del mio percorso di formazione presso l'Istituto Agnelli, abbiamo approfondito una serie di argomenti di grande importanza e attualità. In particolare, ci siamo concentrati su tre temi principali: Creazione di un cv con lettera di presentazione, i rischi dei videogames e dei social (inglese) e le prigioni
+        </p>
+
+        <hr> 
+
+        <h4>cv con relativa lettera di presentazione </h4>
+        <p>Il curriculum vitae presentato insieme alla lettera di presentazione è stato utilizzato anche per le aziende che sono venute in visita presso la scuola.</p>
+        
+        <div style="width: 100%; height: 500px; margin-top: 20px;">
+            <iframe src="/cvformatoeuropeoB.pdf" width="100%" height="500"></iframe>
+        </div>
+        
+        <h4> lettera di presentazione: </h4>
+        <p>Spettabile Direzione del Personale
+            Io sottoscritto Farfariello Emanuele Filippo, mi presento con entusiasmo come candidato per la posizione presso la vostra stimata azienda. La mia esperienza e competenza nel campo informatico unita alla mia dedizione al lavoro di squadra e al servizio al cliente, mi rendono un professionista ideale per contribuire al successo della vostra organizzazione.
+            Negli ultimi mesi, ho avuto il privilegio di lavorare in contesti dinamici e stimolanti, affinando le mie abilità  nel lavorare efficacemente in team (come precedente esperienza lavorativa presso studio DLM).
+            La mia esperienza nel settore mi ha anche permesso di sviluppare competenze specifiche nel dropshipping, che credo sarebbero un valore aggiunto per la vostra azienda. Sono sempre alla ricerca di opportunità  per migliorare e crescere professionalmente, e sono convinto che la vostra azienda offra l'ambiente ideale per continuare a sviluppare le mie competenze e contribuire al vostro successo. 
+            Sto conseguendo le lezioni di teoria per la patente di categoria B, che sarebbe ottimale per lo spostamento di tipo aziendale.
+            Sono entusiasta all'idea di contribuire al vostro team  e di mettere a disposizione la mia esperienza e il mio impegno. Ringrazio anticipatamente per la considerazione della mia candidatura e sono disponibile per un colloquio che sia orale o meno, in cui potremmo discutere in dettaglio come posso apportare un valore del vostro team.
+            Grazie per l'attenzione e resto a disposizione per ulteriori informazioni.     
+            Cordiali saluti,
+            Emanuele Filippo Farfariello
+            </p>
+
+        <hr>
+
+        <h4>the risks and dangers of the video games, of video games, social networks and smartphone</h4>
+        <p>
+            Nella mia presentazione, ho enumerato i pericoli associati all'uso dei social media, dei videogiochi e dei telefoni cellulari.
+        </p>
+
+        <div style="width: 100%; height: 500px; margin-top: 20px;">
+            <iframe src="/the risks and dangers of video games,.pdf" width="100%" height="500"></iframe>
+
+        </div>
+
+
+        <hr>
+        <h4>le prigioni</h4>
+        <p>
+            Nel contesto delle prigioni, si affronta la complessa realtà del sistema carcerario attuale, evidenziando tutti i disagi e le problematiche presenti. In particolare, si esamina la situazione delle prigioni collocate al di fuori delle città, esplorando le implicazioni sociali, economiche e logistiche di tale scelta. Si pone l'attenzione sui vari fattori che contribuiscono alla sovraffollamento carcerario, alla mancanza di risorse e alla difficoltà di riabilitazione dei detenuti. Si esplora anche l'impatto che la collocazione delle prigioni al di fuori delle aree urbane ha sulla vita dei detenuti, sulle loro famiglie e sulle comunità circostanti. Questo approfondimento mira a stimolare una riflessione critica sul sistema penitenziario e a promuovere un dibattito costruttivo su possibili soluzioni e miglioramenti.
+        </p>
+        <div style="width: 100%; height: 500px; margin-top: 20px;">
+            <iframe src="/prigioni.pdf" width="100%" height="500"></iframe>
+
+        </div>
+        <hr>
+        
+
+    </div>
+</body>
+</html>
+    
